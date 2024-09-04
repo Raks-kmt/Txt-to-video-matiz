ef03420fc03776342b537f53ef891b6e22fbf26d
Say something <163742930+Raks-kmt@users.noreply.github.com>
2024-09-04 23:11:30 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "28526237"
+api_hash = "936db76a74f9a52cfb2cea8a62e4c20e"
+bot_token = "7303142725:AAHVCPmVsQs4SFdbXHDPbhXj-mm0ZxfAZ_g"
